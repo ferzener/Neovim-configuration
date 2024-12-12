623e366d1754a8872d26875c6edb5cc0f9bb43bc
Ferzener <ferzener+github@proton.me>
2024-12-12 12:55:31 +0000

    starting setup file

---
+++ setup.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/.config/nvim
+cp -R nvimconf/* ~/.config/nvim
+
+# installing packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
