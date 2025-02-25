83e0d3ea2b1ef67a3830d5dbe5c5b856bf24ebfc
Ferzener <ferzener+github@proton.me>
2025-02-25 20:50:46 +0000

    Update packer.lua

--- nvimconf/lua/ferzener/packer.lua
+++ nvimconf/lua/ferzener/packer.lua
@@ -8,6 +8,8 @@ return require('packer').startup(function(use)
 		
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
+  use 'mbbill/undotree'
+		
   use({'neovim/nvim-lspconfig'})
   use({'hrsh7th/nvim-cmp'})
   use({'hrsh7th/cmp-nvim-lsp'})
